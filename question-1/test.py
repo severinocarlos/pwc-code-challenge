7c7e3e4297071a688c4b94ab0e1339cb6b64f0bc
severinocarlos <juninho_456@hotmail.com>
2023-07-06 00:17:46 -0300

    add: teste suite

---
+++ question-1/test.py
@@ -0,0 +1,15 @@
+import unittest
+from exercice import reverse_string
+
+class TestSuite(unittest.TestCase):
+    def test_case_one(self):
+        self.assertEqual(reverse_string('Hello, World! OpenAI is amazing.'), 'amazing. is OpenAI World! Hello,')
+
+    def test_case_two(self):
+        self.assertEqual(reverse_string('PWC | Code Challenge'), 'Challenge Code | PWC')
+    
+    def test_case_three(self):
+        self.assertEqual(reverse_string('Code, Coffee and Algorithm!!'), 'Algorithm!! and Coffee Code,')
+
+if __name__ == '__main__':
+    unittest.main()
