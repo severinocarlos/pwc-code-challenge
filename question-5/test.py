168c7c69023bbf7af0b896ea451b90881c71ec17
severinocarlos <juninho_456@hotmail.com>
2023-07-06 11:30:39 -0300

    fix: test case

--- question-5/test.py
+++ question-5/test.py
@@ -1,16 +1,22 @@
 import unittest
-from exercise import solve
+from question import solve
 
 class TestSuit(unittest.TestCase):
     def test_one_case(self):
         _input = 'racecar'
         expected_output = 'racecar'
-        self.assertEqual(solve(_input), expected_output)
+
+        output = solve(_input)
+        print(f'Case:\nOutput: {output}    |   Expected Output: {expected_output}\n')
+        self.assertEqual(output, expected_output)
 
     def test_two_case(self):
         _input = 'aabcaab'
         expected_output = 'baacaab'
-        self.assertEqual(solve(_input), expected_output)
+
+        output = solve(_input)
+        print(f'Case:\nOutput: {output}    |   Expected Output: {expected_output}\n')
+        self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
